d8e440e03632155018d6aae105985d857359910a
larissaRF <larissa_lrf@hotmail.com>
2024-01-21 18:22:22 -0300

    list

--- projeto-final/main.js
+++ projeto-final/main.js
@@ -1,1 +1,11 @@
+import { addSubmitResultListener } from './src/results'
 import './style.css'
+
+document.addEventListener("DOMContentLoaded", function () {
+  const newResultForm = document.getElementById("newResultForm")
+  if (newResultForm) addSubmitResultListener(newResultForm)
+
+  const resultsList = document.getElementById("resultsList")
+  if (resultsList) loadResults(resultsList)
+  
+})
